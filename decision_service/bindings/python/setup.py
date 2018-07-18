7ae580f7fe91926fcfb8aaf40c91b2ecb95dc5ae
Alexey Taymanov <ataymano@gmail.com>
2018-07-18 17:04:05 -0400

    Pip packages files restructuring, setup fixes

--- decision_service/bindings/python/setup.py
+++ decision_service/bindings/python/setup.py
@@ -5,11 +5,12 @@ with open("README.md", "r") as fh:
 	long_description = fh.read()
 
 extension_module = setuptools.Extension(
-	'rlinference.extension',
-	sources = glob.glob('*_wrap.cc'),
-	library_dirs = ['../../../rlclientlib/'],
-	include_dirs = ['../../../include/'],
-	libraries = ['rlclient']
+	'rlinference._rlinference',
+	sources = glob.glob('*.cc'),
+	library_dirs = ['../../rlclientlib/', '../../../vowpalwabbit/'],
+	include_dirs = ['../../include/'],
+	libraries = ['rlclient', 'vw', 'allreduce', 'boost_system', 'boost_program_options', 'cpprest', 'ssl', 'crypto', 'z', 'pthread', 'dl'],
+	extra_compile_args = ['-std=c++11'],
 )
 
 setuptools.setup(
@@ -19,9 +20,9 @@ setuptools.setup(
 	author_email = "ataymano@gmail.com",
 	description = "A small example",
 	long_description = long_description,
-	long_description_content_type = "text/markdown",
 	url = "https://github.com/ataymano/hello_world_12.git",
 	ext_modules = [extension_module],
+	py_modules = ['rlinference.py'],
 	packages = setuptools.find_packages(),
 	classifiers = (
 		"Programming Language :: Python :: 3",
